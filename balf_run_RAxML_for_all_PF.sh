5c0d458e93c20a81d58c08d45e25cbb2d180361a
Justin C. Bagley <justincbagley@users.noreply.github.com>
2018-05-13 21:30:55 -0400

    Create balf_run_RAxML_for_all_PF.sh

---
+++ balf_run_RAxML_for_all_PF.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+
+## Script for conditionally setting up and running basic, full BS and ML search run in
+## SSE3 HPC MPI version of RAxML v8 (Stamatakis 2014) in the 'raxml' subdirectory of each
+## model run folder (e.g. ./M9_10/, is model M9 run 10) for balf_clade.
+
+## USAGE: ./balf_run_RAxML_for_all_PF.sh <email>
+
+## Alignment file: balf_clade_simData.phy
+## PF partitions file: balf_clade_simData.PF.partitions
+
+
+######################################## START ###########################################
+echo "INFO      | $(date) | Starting balf_run_RAxML_for_all_PF analysis... "
+echo "INFO      | $(date) | STEP #1: SETUP AND USER INPUT. "
+###### Set paths, environmental variables, and filetypes as different variables:
+	MY_PATH=`pwd -P`
+#echo "INFO      | $(date) |          Setting working directory to: $MY_PATH "
+	CR=$(printf '\n')
+	calc () { 
+	bc -l <<< "$@"; 
+}
+
+###### Set environmental variable for user input:
+MY_EMAIL_ACCOUNT=$1
+
+
+echo "INFO      | $(date) | STEP #2: LOOP THROUGH RAxML FOLDERS, CONDITIONALLY SET UP AND RUN RAxML IF FILE STRUCT & QUEUE RESOURCES AVAILABLE. "
+(
+	for i in ./*/; do
+		cd "$i";
+		MY_SET_FOLDERNAME="$(basename $j)"
+		echo "INFO      | $(date) | ##----------   $MY_SET_FOLDERNAME"   ----------##; 
+	
+		for j in ./*/; do
+			cd "$j";
+			MY_FOLDERNAME="$(basename $j)"
+			echo "INFO      | $(date) | ...  $MY_FOLDERNAME  ...";
+			(
+				if [[ -f ./PartitionFinder/analysis/best_scheme.txt  ]] && [[ "$(wc -c ./PartitionFinder/analysis/best_scheme.txt | sed 's/\ .*//' | perl -pe $'s/\t//g')" -gt 10000 ]]; then 
+					cd ./raxml/; 
+						echo "INFO      | $(date) | Prepping ${MY_FOLDERNAME} RAxML run folder... "
+						if [[ ! -f ./balf_clade_simData.phy ]]; then 
+							cp ../ElConcatenero/balf_clade_simData.phy .;
+						fi
+
+						if [[ ! -f ./balf_clade_simData.PF.partitions ]]; then
+							MY_TOTAL_NUM_LINES="$(cat ../PartitionFinder/analysis/best_scheme.txt | wc -l | sed 's/\ //g')"
+							MY_RAXML_PART_START_LINE="$(cat best_scheme.txt | grep -n 'RaxML' | sed 's/:.*//')"
+							sed -n ''"$MY_RAXML_PART_START_LINE"','"$MY_TOTAL_NUM_LINES"'p' ../PartitionFinder/analysis/best_scheme.txt > ./balf_clade_simData.PF.partitions
+						fi
+
+						if [[ ! -f ./raxml_qsub.sh ]]; then
+echo "INFO      | $(date) | Creating raxml run (shell) script... "
+echo "#!/bin/bash
+
+#$ -N ${MY_FOLDERNAME}
+#$ -S /bin/bash
+#$ -cwd
+#$ -V
+#$ -j y
+##  #$ -pe smp 4
+#$ -m a
+#$ -M ${MY_EMAIL_ACCOUNT}
+
+#---Change walltime to be the expected number of hours for the run-------------#
+#---NOTE: The run will be killed if this time is exceeded----------------------#
+#---Change the -M flag to point to your email address.-------------------------#
+
+
+cd ${PWD}
+
+raxml -f a -x $(python -c "import random; print random.randint(10000,100000000000)") -p $(python -c "import random; print random.randint(10000,100000000000)") -# 100 -m GTRGAMMA -s ./balf_clade_simData.phy -q ./balf_clade_simData.PF.partitions -n balf_${MY_FOLDERNAME}_raxml
+
+
+exit 0
+
+" > ./raxml_qsub.sh
+
+						fi
+
+						## Check for queue slots to use for raxml runs (depends on custom q stat check function I wrote called 'myq'):
+						MY_REMAINING_QUEUE_SLOTS="$(myq | grep -h 'Remaining\ slots' | sed 's/Remain.*\ //')"
+						if [[ "$MY_REMAINING_QUEUE_SLOTS" -ge "1" ]]; then
+							qsub ./raxml_qsub.sh/;
+						elif [[ "$MY_REMAINING_QUEUE_SLOTS" ! -ge "1" ]]; then
+							echo "WARNING!  | $(date) | Insufficient slots remaining to queue ${MY_FOLDERNAME} RAxML run. Quitting... "
+							exit 1
+						fi
+
+					cd ..;
+				
+				
+				elif [[ ! -f ./PartitionFinder/analysis/best_scheme.txt ]]; then
+					echo "WARNING!  | $(date) | PartitionFinder run for ${MY_FOLDERNAME} is incomplete. Moving on to next run folder... "
+				fi
+			)
+			cd ..;
+		done
+	
+		cd ..;
+	done
+)	
+
+
+exit 0
+
